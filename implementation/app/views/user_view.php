6ecb2e130a8af565a3168053b6ebfe3447109e95
Mahmoud Ahmed <mahmoud_ahmed@stud.fci-cu.edu.eg>
2020-04-02 19:24:10 +0200

    Add files via upload

--- implementation/app/views/user_view.php
+++ implementation/app/views/user_view.php
@@ -4,6 +4,11 @@ class UserView extends View
 {
 	public function renderArray($_users)
 	{
+		// putting data to JSONObject 
+		$data["status"] = "ok";
+		$data["data"] = $_users;
+
+		return json_encode($data, JSON_PRETTY_PRINT);
 	}
 }
 
--- implementation/app/views/view.php
+++ implementation/app/views/view.php
@@ -4,17 +4,29 @@ class View
 {
 	public function renderObject($_key, $_value)
 	{
+		// putting data to JSONObject 
+        $data = array("status" => "ok", $_key => $_value);
+        return json_encode($data, JSON_PRETTY_PRINT);
 	}
 
     public function success()
     {
+    	// putting data to JSONObject 
+        $data = array("status" => "ok", "message" => "the request has succeeded.");
+        return json_encode($data, JSON_PRETTY_PRINT);
     }
 
     public function fail()
     {
+    	// putting data to JSONObject 
+        $data = array("status" => "fail", "message" => "the request has failed. Please review the data sent.");
+        return json_encode($data, JSON_PRETTY_PRINT);
     }
 
     public function exception()
     {
+    	// putting data to JSONObject 
+        $data = array("status" => "exception", "message" => "unsupported get request. Please read the API documentation.");
+        return json_encode($data, JSON_PRETTY_PRINT);
     }
 }
