dd7cd83d213c3249a153c8ee8ee0007caf0cff1c
Mohamed A. Abd El-Nabey <51803872+MOo-Ahmed@users.noreply.github.com>
2020-04-02 12:09:38 +0200

    Update Router

--- implementation/bootstrap/router.php
+++ implementation/bootstrap/router.php
@@ -7,9 +7,43 @@ class Router
 
 	public function __construct($_GETRequest)
     {
+        $this->GETRequest = $_GETRequest;
+
+        // Fill the array with the names of the classes
+        $this->controllersClassesArr = array();
+        $this->controllersClassesArr[] = "UserController";
     }
 
     public function route()
     {
+    	if(!array_key_exists("class", $this->GETRequest))
+    	{
+        	return false;
+    	}
+
+        if(!array_key_exists("method", $this->GETRequest))
+        {
+            return false;
+        }
+
+        if(!array_key_exists("parameters", $this->GETRequest))
+        {
+            return false;
+        }
+
+        if(!in_array($this->GETRequest["class"], $this->controllersClassesArr))
+        {
+            return false;
+        }
+  
+        $this->controller = new $this->GETRequest["class"]($this->GETRequest["parameters"]);
+
+        if($this->controller->methodExistsCheck($this->GETRequest["method"]))
+        {
+            call_user_func(array($this->controller, $this->GETRequest["method"]));
+            return true;
+        }
+            
+        return false;
     }
 }
