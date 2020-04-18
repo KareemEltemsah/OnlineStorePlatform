d1e0f33a190f5b022ef32ca0ae5f3c196b24d271
Kareem Eltemsah <36171346+KareemEltemsah@users.noreply.github.com>
2020-04-18 18:28:24 +0200

    update data_access_object

--- implementation/app/utilities/data_access_object.php
+++ implementation/app/utilities/data_access_object.php
@@ -2,10 +2,47 @@
 
 class DataAccessObject
 {
-	private $db = null;
-	private $tableName;
+	public $db = null;
+	public $tableName;
+	public $columns = array();
 
-	public function getTableName()
+	public function __construct($_tableName)
 	{
+		$this->tableName = $GLOBALS['configs']["tables"][$_tableName];
+        $this->columns = $GLOBALS['configs']["columns"][$_tableName];
+		$this->db = DatabaseConnector::getInstance()->getConnection();
+	}
+
+	public function select($_array)
+	{
+		$i = 0;
+		$arrayCount = count($_array);
+		$query = "SELECT * FROM `" . $this->tableName . "` WHERE ";
+
+
+		foreach($_array as $key => $value)
+		{
+			$value = $this->db->real_escape_string($value);
+			
+
+			if(++$i === $arrayCount) 
+			{
+				$query .= '`' . $key . '` = \'' . $value . "'";
+			}
+			else
+			{
+				$query .= '`' . $key . '` = \'' . $value . "' AND ";
+			}
+		}
+
+      		// If the query can't be executed (e.g: use of special characters in inputs)
+        	if(!$result = $this->db->query($query)) 
+		{
+            		return 0;
+        	}
+
+        	$user = $result->fetch_assoc();
+
+        	return $user;
 	}
 }
